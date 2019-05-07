25a4cb00e383d9cf44ac0c0719ca8f6726545fda
Benjamin <hoppeconnect@gmail.com>
2019-05-07 09:47:17 -0400

    navbar not rendering at 20 mins

--- src/ components/Navbar/Navbar.js
+++ src/ components/Navbar/Navbar.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const NavBar = () => {
-  return(
-    <div>
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 6rem;
+`; 
 
-
-    </div>
-  )
-}
+const Navbar = () => {
+  return <Wrapper>Bye</Wrapper>;
+};
 
 export default Navbar;
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import Navbar from '../components/Navbar/Navbar';
 import styled from 'styled-components';
+import Navbar from '../components/Navbar/Navbar';
 
 
-const MainWrapper = style.div`
+const MainWrapper = styled.div`
   width: 100%;
   min-height: calc(100vh - 6rem);
+  margin-top: 6rem;
 `;
 
 const Layout = ({children}) => (
   <>
-    <Navbar/>
+    <Navbar />
     <MainWrapper>{children}</MainWrapper>
   </>
 );
